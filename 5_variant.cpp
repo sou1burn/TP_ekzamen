9e8a42a039fba6706ab2de9af6dcdf935ba2942d
sou1burn <koryakinv_04@mail.ru>
2024-06-19 01:38:03 +0300

    дописал 6 вариант, осталось 4 в пятом

--- 5_variant.cpp
+++ 5_variant.cpp
@@ -37,11 +37,66 @@ int unique_strings(std::list<std::string> lst) {
 	return set_str.size();
 }
 
+class Node {
+public:
+	int val;
+	std::vector<Node*> childs;
+
+	Node(int val) : val(val) {}
+};
+
+
+void find_nodes_with_3childs(Node* root) {
+	
+	if (root == nullptr) {
+		return;
+	}
+
+	if (root->childs.size() == 3) {
+		std::cout << "Node with 3 childs is : " << root->val <<"\n";
+	}
+
+	for (Node* child : root->childs) {
+		find_nodes_with_3childs(child);
+	}
+}
+
 int main() {
 
 	int src[] = { 1, 2, 3, 4, 5 };
 	size_t length = sizeof(src) / sizeof(src[0]);
 	exchanger(src, length);
 
+
+	Node* root = new Node(1);
+	root->childs.push_back(new Node(2));
+	root->childs.push_back(new Node(3));
+	root->childs.push_back(new Node(4));
+
+	root->childs[0]->childs.push_back(new Node(5));
+	root->childs[0]->childs.push_back(new Node(6));
+	root->childs[0]->childs.push_back(new Node(7));
+
+	root->childs[1]->childs.push_back(new Node(8));
+	root->childs[1]->childs.push_back(new Node(9));
+	root->childs[1]->childs.push_back(new Node(10));
+
+	root->childs[2]->childs.push_back(new Node(11));
+
+	find_nodes_with_3childs(root);
+
+	delete root->childs[0]->childs[0];
+	delete root->childs[0]->childs[1];
+	delete root->childs[0]->childs[2];
+	delete root->childs[0];
+
+	delete root->childs[1]->childs[0];
+	delete root->childs[1]->childs[1];
+	delete root->childs[1];
+
+	delete root->childs[2]->childs[0];
+	delete root->childs[2];
+
+	delete root;
 	return 0;
 }
--- 6_variant.cpp
+++ 6_variant.cpp
@@ -43,6 +43,104 @@ int most_frequent(std::list<int> m) {
 	return most_frequent;
 }
 
+bool find_sum(std::vector<int> nums, int curr_id, int curr_sum, int sum) {
+
+    if (curr_id == nums.size()) {
+        return curr_sum == sum;
+    }
+
+    if (find_sum(nums, curr_id + 1, curr_sum + nums[curr_id], sum)) {
+        return true;
+    }
+
+    if (find_sum(nums, curr_id + 1, curr_sum - nums[curr_id], sum)) {
+        return true;
+    }
+
+    return false;
+}
+
+bool can_find_sum(int s, std::vector<int> nums) {
+
+    return find_sum(nums, 0, 0, s);
+}
+
+
+struct Node {
+    int value;
+    Node* left;
+    Node* right;
+
+    Node(int val) : value(val), left(nullptr), right(nullptr) {}
+};
+
+template <typename T>
+class Stack {
+public:
+    Stack(size_t capacity) : top(-1), capacity(capacity) {
+        stack = new T[capacity];
+    }
+
+    ~Stack() {
+        delete[] stack;
+    }
+
+    void push(T value) {
+        if (top < capacity - 1) {
+            stack[++top] = value;
+        }
+        else {
+            std::cerr << "\nStack overflow" << std::endl;
+        }
+    }
+
+    T pop() {
+        if (top >= 0) {
+            return stack[top--];
+        }
+        else {
+            std::cerr << "\nStack underflow" << std::endl;
+            return nullptr; 
+        }
+    }
+
+    bool isEmpty() {
+        return top == -1;
+    }
+
+private:
+    T* stack;
+    int top;
+    size_t capacity;
+};
+
+
+void printTreeNodes(Node* root) {
+
+    if (root == nullptr) {
+        return;
+    }
+
+    Stack<Node*> stack(100); 
+
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+        
+        Node* current = stack.pop();
+        std::cout << current->value << " ";
+
+        
+        if (current->right != nullptr) {
+            stack.push(current->right);
+        }
+
+        
+        if (current->left != nullptr) {
+            stack.push(current->left);
+        }
+    }
+}
 
 int main() {
 	try {
@@ -61,6 +159,35 @@ int main() {
 }
 	std::list<int> nums = { 1, 2, 2, 3, 3, 3, 3 ,3 , 4, 4, 4, 4 };
 	std:: cout << "Most frequent: "<< most_frequent(nums);
+
+    std::vector<int> numbers = { 1, 2, 3, 4, 5 };
+    int target_sum = 15;
+
+    if (can_find_sum(target_sum, numbers)) {
+        std::cout << "\nIt is possible to form the sum " << target_sum << " using + and - between the numbers." << std::endl;
+    }
+    else {
+        std::cout << "\nIt is not possible to form the sum " << target_sum << " using + and - between the numbers." << std::endl;
+    }
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+
+    printTreeNodes(root);
+
+    delete root->left->left;
+    delete root->left->right;
+    delete root->right->left;
+    delete root->right->right;
+    delete root->left;
+    delete root->right;
+    delete root;
 
 	return 0;
 }
